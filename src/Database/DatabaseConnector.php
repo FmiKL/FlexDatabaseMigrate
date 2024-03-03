28ecfdf1ef73e56eb7563a775fd5202dba50f20f
Mikael Fourré <mikael6287@live.fr>
2024-03-03 15:27:27 +0100

    Create DatabaseConnector class

---
+++ src/Database/DatabaseConnector.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace FlexDatabaseMigrate\Database;
+
+use mysqli;
+
+class DatabaseConnector {
+    private mysqli $connection;
+
+    public function __construct(array $config)
+    {
+        $this->connection = new mysqli($config['host'], $config['username'], $config['password'], $config['database']);
+        if ($this->connection->connect_error) {
+            die('Connection failed: ' . $this->connection->connect_error);
+        }
+    }
+
+    public function getConnection(): mysqli
+    {
+        return $this->connection;
+    }
+}
