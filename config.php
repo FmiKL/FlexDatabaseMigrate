1f4d2ebdbb80a69dd9c1fdb66c8e2423605349e6
Mikael Fourré <mikael6287@live.fr>
2024-03-03 15:22:43 +0100

    Create config file

---
+++ config.php
@@ -0,0 +1,15 @@
+<?php
+
+define('DB_SOURCE', [
+    'host' => 'localhost',
+    'username' => 'root',
+    'password' => 'root',
+    'database' => 'database_source'
+]);
+
+define('DB_TARGET', [
+    'host' => 'localhost',
+    'username' => 'root',
+    'password' => 'root',
+    'database' => 'database_target'
+]);
